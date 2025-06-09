5559d4c84bcc843034dfd8259647b3d5866afb2f
archerriesh <2472014@maranatha.ac.id>
2025-06-09 11:04:31 +0700

    maknaan ada navbar

--- makanan.js
+++ makanan.js
@@ -1,3 +1,21 @@
+document.querySelectorAll('.navbar-nav .nav-link').forEach(link => {
+      link.addEventListener('click', () => {
+        const navbarCollapse = document.querySelector('.navbar-collapse');
+        if (navbarCollapse.classList.contains('show')) {
+          new bootstrap.Collapse(navbarCollapse).hide();
+        }
+      });
+    });
+
+    const contactForm = document.querySelector('footer form');
+    contactForm.addEventListener('submit', e => {
+      e.preventDefault(); 
+      if(confirm('Halo!Sudah siap mengirim pesan?')) {
+        alert("pesan kamu sudah dikirim!Terima kasih-!");
+        contactForm.reset();
+      }
+    });
+
 fetch("makanan.json")
   .then((res) => res.json())
   .then((reks) => {
